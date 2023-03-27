babdfa88c637233bbd65f749e857416034a07965
Kostuyra <gkv110592@yandex.ru>
2023-03-27 12:41:10 +0300

    Выполнено дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+
+		taskOne(true);
+		taskTwo(taskOne(false));
+		taskThree();
 	}
+
+	static String taskOne(boolean main) {
+
+		if (main) {
+			System.out.println("Задача 1");;
+		}
+
+		String firstName = "Ivan";
+		String middleName = "Ivanovich";
+		String lastName = "Ivanov";
+		String fullName = lastName + ' ' + firstName + ' ' + middleName;
+		if (main) {
+			System.out.println("ФИО сотрудника — " + fullName);
+		}
+		return fullName;
+	}
+
+
+	static void taskTwo(String fullName) {
+		System.out.println("Задача 2");
+		fullName = fullName.toUpperCase();
+		System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName);
+	}
+
+	static void taskThree() {
+		System.out.println("Задача 3");
+		String fullName = "Иванов Семён Семёнович";
+		fullName = fullName.replace("ё","е");
+		System.out.println("Данные ФИО сотрудника — " + fullName);
+	}
+
 }
